72c167f94e7a62bd9d98f09a6ce018b55bfc4e96
cynjean <cynthia.2056@yahoo.com>
2026-07-25 14:19:48 -0500

    Add local storage form persistence

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,15 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [value, setValue] = useState(() => {
+        const storedValue = localStorage.getItem(key);
+        return storedValue || initialValue;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, value);
+
+    }, [key, value]);
+
+    return [value, setValue];
 }
